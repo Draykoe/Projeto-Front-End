d8626348ae4cb7395ca83309d2dff0324c63bc69
Draykoe <roberto.morita@hotmail.com>
2021-09-04 17:17:31 -0300

    01

---
+++ _javascript/01.js
@@ -0,0 +1,21 @@
+//comentario de linha
+
+//console.log('Olá mundo!!!');
+
+//var nome = 'Roberto';
+//var sobrenome = ' Morita';
+//var nomecompleto = nome + sobrenome;
+//var Idade = ' 28 anos';
+
+//console.log(nomecompleto + Idade);
+
+var valor1 = 10;
+var valor2 = 2;
+var total;
+//total = valor1 + valor2;
+//total = valor1 - valor2;
+//total = valor1 * valor2;
+//total = valor1 / valor2;
+//console.log(total);
+//total = valor1 ** valor2;
+//total = valor1 % valor2;
---
+++ _javascript/02.js
@@ -0,0 +1,6 @@
+var valor1 = 4
+var valor2 = 5
+var valor3 = 9
+//var total = (valor1 + valor2) * valor3;
+//console.log(total);
+total = valor1
